892b14b4ed1a444fe0bb57213ab4ddeff3938e6b
Arianna J Savant <ariannasavant@gmail.com>
2014-10-13 17:05:06 -0400

    added/refactored portfolio-level analytics methods

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -2,19 +2,28 @@ class Portfolio < ActiveRecord::Base
 	has_and_belongs_to_many :liens
 	has_many :properties, through: :liens
 
-	def average_LTV
-		
+	def total(field_as_sym)
+		return liens.pluck(field_as_sym).reduce(:+)
 	end
 
-	def total_current_rv(date = Date.today)
-		return liens.pluck(:rv).reduce(:+)
+	def avg(field_as_sym)
+		return liens.average(field_as_sym).round(2)
 	end
 
-	def total_fv
-		return liens.pluck(:fv).reduce(:+)
+	def min(field_as_sym)
+		return liens.minimum(field_as_sym).round(2)
+	end
+
+	def max(field_as_sym)
+		return liens.maximum(field_as_sym).round(2)
 	end
 
 	def total_accrued_interest(date = Date.today)
 		return liens.map { |l| l.accrued_interest(date) }.reduce(:+)
 	end
+
+	def interest_rate_distribution(options={})
+		
+	end
+
 end
